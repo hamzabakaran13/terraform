f3fd7ac38ce98c566b403485ef97c124015588a3
hamzabakaran13 <hamza.bakaran@allops.co>
2025-08-21 15:20:51 +0200

    Refactoring for automatisation

--- terraform-code/modules/repos/main.tf
+++ terraform-code/modules/repos/main.tf
@@ -21,15 +21,10 @@ module "deploy_keys" {
   repo_name = each.key
 }
 
-output "repo_info" {
-  value = { for k, v in module.repos : k => v.clone-urls }
-}
 output "repo-list" {
   value = flatten([for k, v in module.repos : keys(v.clone-urls) if k == "dev"])
 }
 
-# module "info_page" {
-#  source          = "./modules/info-page"
-#  repos           = { for k, v in module.repos["prod"].clone-urls : k => v }
-#  run_provisoners = false
-# }
+output "clone_urls" {
+  value = module.repos
+}
